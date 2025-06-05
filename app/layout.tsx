8e57da33f0c66006d7c3054f50ae1da00473ee68
Tu Nombre <tu.email@ejemplo.com>
2025-06-05 02:23:42 -0400

    Remove favicon configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,9 +20,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es" suppressHydrationWarning>
-      <head>
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
